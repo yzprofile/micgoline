98552c887762ba4b585398074bfab3f095854b50
yzprofile <yzprofiles@gmail.com>
2016-02-20 13:48:36 +0800

    micgoline

---
+++ micgoline-autoloads.el
@@ -0,0 +1,14 @@
+;;; micgoline-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; micgoline-autoloads.el ends here
